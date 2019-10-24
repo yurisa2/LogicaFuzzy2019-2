9feb5562805d337a3b7b4e5d3ee4cd7702914baf
@yurisa2 - SuperFast <yuri@sa2.com.br>
2019-10-24 10:45:06 -0300

    Updated Sets

--- ArtigoFinal/FinalRC1.R
+++ ArtigoFinal/FinalRC1.R
@@ -31,21 +31,21 @@ fis_TP <- newfis("Tanque Percola��o")
 #adiciona variaveis
 fis_TP <- addvar(fis_TP, "input", "CN", c(0,100))
 fis_TP <- addvar(fis_TP, "input", "Declividade", c(0,160))
-fis_TP <- addvar(fis_TP, "input", "Permeabilidade", c(0,1))
+fis_TP <- addvar(fis_TP, "input", "Permeabilidade", c(0,1000))
 fis_TP <- addvar(fis_TP, "output", "TPercolacao", c(0,1))
 
 #Cria Membership Functions
-fis_TP <- addmf(fis_TP, "input", 1, "baixa", "trimf", c(0,0,60))
-fis_TP <- addmf(fis_TP, "input", 1, "media", "trimf", c(0,60,100))
-fis_TP <- addmf(fis_TP, "input", 1, "alta", "trimf", c(60,100,100))
+fis_TP <- addmf(fis_TP, "input", 1, "baixa", "trapmf", c(0,0,40,55))
+fis_TP <- addmf(fis_TP, "input", 1, "media", "trapmf", c(40,55,60,75))
+fis_TP <- addmf(fis_TP, "input", 1, "alta", "trapmf", c(60,75,100,100))
 
-fis_TP <- addmf(fis_TP, "input", 2, "baixa", "trimf", c(0,0,30))
-fis_TP <- addmf(fis_TP, "input", 2, "media", "trimf", c(0,30,160))
-fis_TP <- addmf(fis_TP, "input", 2, "alta", "trimf", c(30,160,160))
+fis_TP <- addmf(fis_TP, "input", 2, "baixa", "trapmf", c(0,0,5,6))
+fis_TP <- addmf(fis_TP, "input", 2, "media", "trapmf", c(5,6,20,30))
+fis_TP <- addmf(fis_TP, "input", 2, "alta", "trapmf", c(20,30,160,160))
 
-fis_TP <- addmf(fis_TP, "input", 3, "baixa", "trimf", c(0,0,0.5))
-fis_TP <- addmf(fis_TP, "input", 3, "media", "trimf", c(0,0.5,1))
-fis_TP <- addmf(fis_TP, "input", 3, "alta", "trimf", c(0.5,1,1))
+fis_TP <- addmf(fis_TP, "input", 3, "baixa", "trapmf", c(0,0,50,150))
+fis_TP <- addmf(fis_TP, "input", 3, "media", "trapmf", c(50,150,225,400))
+fis_TP <- addmf(fis_TP, "input", 3, "alta", "trapmf", c(225,400,1000,1000))
 
 fis_TP <- addmf(fis_TP, "output", 1, "baixa", "trimf", c(0,0,0.5))
 fis_TP <- addmf(fis_TP, "output", 1, "media", "trimf", c(0,0.5,1))
@@ -106,23 +106,23 @@ fis_TA <- newfis("Tanque Agricola")
 
 
 #adiciona variaveis
-fis_TA <- addvar(fis_TA, "input", "CN", c(0,100))
+fis_TP <- addvar(fis_TA, "input", "CN", c(0,100))
 fis_TA <- addvar(fis_TA, "input", "Declividade", c(0,160))
-fis_TA <- addvar(fis_TA, "input", "Permeabilidade", c(0,1))
+fis_TA <- addvar(fis_TA, "input", "Permeabilidade", c(0,1000))
 fis_TA <- addvar(fis_TA, "output", "TPercolacao", c(0,1))
 
 #Cria Membership Functions
-fis_TA <- addmf(fis_TA, "input", 1, "baixa", "trimf", c(0,0,60))
-fis_TA <- addmf(fis_TA, "input", 1, "media", "trimf", c(0,60,100))
-fis_TA <- addmf(fis_TA, "input", 1, "alta", "trimf", c(60,100,100))
+fis_TA <- addmf(fis_TA, "input", 1, "baixa", "trapmf", c(0,0,40,55))
+fis_TA <- addmf(fis_TA, "input", 1, "media", "trapmf", c(40,55,60,75))
+fis_TA <- addmf(fis_TA, "input", 1, "alta", "trapmf", c(60,75,100,100))
 
-fis_TA <- addmf(fis_TA, "input", 2, "baixa", "trimf", c(0,0,30))
-fis_TA <- addmf(fis_TA, "input", 2, "media", "trimf", c(0,30,160))
-fis_TA <- addmf(fis_TA, "input", 2, "alta", "trimf", c(30,160,160))
+fis_TA <- addmf(fis_TA, "input", 2, "baixa", "trapmf", c(0,0,5,6))
+fis_TA <- addmf(fis_TA, "input", 2, "media", "trapmf", c(5,6,20,30))
+fis_TA <- addmf(fis_TA, "input", 2, "alta", "trapmf", c(20,30,160,160))
 
-fis_TA <- addmf(fis_TA, "input", 3, "baixa", "trimf", c(0,0,0.5))
-fis_TA <- addmf(fis_TA, "input", 3, "media", "trimf", c(0,0.5,1))
-fis_TA <- addmf(fis_TA, "input", 3, "alta", "trimf", c(0.5,1,1))
+fis_TA <- addmf(fis_TA, "input", 3, "baixa", "trapmf", c(0,0,50,150))
+fis_TA <- addmf(fis_TA, "input", 3, "media", "trapmf", c(50,150,225,400))
+fis_TA <- addmf(fis_TA, "input", 3, "alta", "trapmf", c(225,400,1000,1000))
 
 fis_TA <- addmf(fis_TA, "output", 1, "baixa", "trimf", c(0,0,0.5))
 fis_TA <- addmf(fis_TA, "output", 1, "media", "trimf", c(0,0.5,1))
