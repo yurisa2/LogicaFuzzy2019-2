27028f1edc3788b5e5e89bf26c51c86c3c45aa63
yurisa2 - DinoWin <yuri@sa2.com.br>
2019-11-01 16:57:36 -0200

    Resultados preliminares

--- ArtigoFinal/FinalRC1.R
+++ ArtigoFinal/FinalRC1.R
@@ -5,28 +5,16 @@ library("FuzzyR")
 setwd("")
 # Tem que colocar a pasta que ele estï¿½, se for no windows, trocar as \ por /
 
-CN <- read_sf("ShapeFiles/pontos_CN.shp")
-Declividade <- read_sf("ShapeFiles/Declividade.shp")
-Permeabilidade <- read_sf("ShapeFiles/permeabilidade.shp")
+camadas_unidas <- read_sf("ShapeFiles/camadas_unidas.shp")
 
-# Pego o mï¿½nimo de linhas dos 3
-n_lines <- min(nrow(CN), nrow(Declividade),  nrow(Permeabilidade))
-
-
-# Aqui eu Deixo todos do mesmo tamanho
-CN <- CN[1:n_lines,]
-Declividade <- Declividade[1:n_lines,]
-Permeabilidade <- Permeabilidade[1:n_lines,]
-
-# Resumï¿½o de cada um
-summary(CN$grid_code)
-summary(Declividade$grid_code)
-summary(Permeabilidade$grid_code)
+CN <- camadas_unidas$CN
+Declividade <- camadas_unidas$Slope
+Permeabilidade <- camadas_unidas$PER
 
 # cria FIS PERCOLACAO COM dois FIS o processamento ï¿½ mais rï¿½pido
 # (mas a gente pode dizer que foi um Sï¿½, sem crise)
 
-fis_TP <- newfis("Tanque Percolaï¿½ï¿½o")
+fis_TP <- newfis("Tanque Percolação")
 
 #adiciona variaveis
 fis_TP <- addvar(fis_TP, "input", "CN", c(0,100))
@@ -58,40 +46,34 @@ plotmf(fis_TP,"input",3)
 plotmf(fis_TP,"output",1)
 
 
-ruleList <- rbind(c(1,1,1,2,1,1),
+ruleList <- rbind(c(1,1,1,1,1,1),
                   c(1,1,2,2,1,1),
                   c(1,1,3,3,1,1),
-
                   c(1,2,1,1,1,1),
                   c(1,2,2,2,1,1),
                   c(1,2,3,3,1,1),
-
                   c(1,3,1,1,1,1),
-                  c(1,3,2,2,1,1),
+                  c(1,3,2,1,1,1),
                   c(1,3,3,3,1,1),
 
                   c(2,1,1,1,1,1),
-                  c(2,1,2,2,1,1),
+                  c(2,1,2,1,1,1),
                   c(2,1,3,3,1,1),
-
                   c(2,2,1,1,1,1),
                   c(2,2,2,2,1,1),
                   c(2,2,3,3,1,1),
-
                   c(2,3,1,1,1,1),
-                  c(2,3,2,2,1,1),
+                  c(2,3,2,1,1,1),
                   c(2,3,3,3,1,1),
 
                   c(3,1,1,1,1,1),
                   c(3,1,2,2,1,1),
                   c(3,1,3,3,1,1),
-
                   c(3,2,1,1,1,1),
-                  c(3,2,2,2,1,1),
+                  c(3,2,2,1,1,1),
                   c(3,2,3,3,1,1),
-
                   c(3,3,1,1,1,1),
-                  c(3,3,2,2,1,1),
+                  c(3,3,2,1,1,1),
                   c(3,3,3,3,1,1)
 
                   )
@@ -99,14 +81,11 @@ ruleList <- rbind(c(1,1,1,2,1,1),
 fis_TP <- addrule(fis_TP,matrix(ruleList,ncol=6)) # Adiciona as regras
 # showrule(fis_TP) # Mostra As regras
 
-showGUI(fis_TP)
-
 #cria FIS TANQUE AGRICOLA
 fis_TA <- newfis("Tanque Agricola")
 
-
 #adiciona variaveis
-fis_TP <- addvar(fis_TA, "input", "CN", c(0,100))
+fis_TA <- addvar(fis_TA, "input", "CN", c(0,100))
 fis_TA <- addvar(fis_TA, "input", "Declividade", c(0,160))
 fis_TA <- addvar(fis_TA, "input", "Permeabilidade", c(0,1000))
 fis_TA <- addvar(fis_TA, "output", "TPercolacao", c(0,1))
@@ -128,21 +107,13 @@ fis_TA <- addmf(fis_TA, "output", 1, "baixa", "trimf", c(0,0,0.5))
 fis_TA <- addmf(fis_TA, "output", 1, "media", "trimf", c(0,0.5,1))
 fis_TA <- addmf(fis_TA, "output", 1, "alta", "trimf", c(0.5,1,1))
 
-plotmf(fis_TA,"input",1)
-plotmf(fis_TA,"input",2)
-plotmf(fis_TA,"input",3)
-
-plotmf(fis_TA,"output",1)
-
 
 ruleList <- rbind(c(1,1,1,2,1,1),
-                  c(1,1,2,2,1,1),
+                  c(1,1,2,1,1,1),
                   c(1,1,3,1,1,1),
-
                   c(1,2,1,3,1,1),
-                  c(1,2,2,2,1,1),
+                  c(1,2,2,1,1,1),
                   c(1,2,3,1,1,1),
-
                   c(1,3,1,3,1,1),
                   c(1,3,2,2,1,1),
                   c(1,3,3,1,1,1),
@@ -150,23 +121,19 @@ ruleList <- rbind(c(1,1,1,2,1,1),
                   c(2,1,1,3,1,1),
                   c(2,1,2,2,1,1),
                   c(2,1,3,1,1,1),
-
                   c(2,2,1,3,1,1),
                   c(2,2,2,2,1,1),
                   c(2,2,3,1,1,1),
-
                   c(2,3,1,3,1,1),
                   c(2,3,2,2,1,1),
                   c(2,3,3,1,1,1),
 
                   c(3,1,1,3,1,1),
-                  c(3,1,2,2,1,1),
+                  c(3,1,2,1,1,1),
                   c(3,1,3,1,1,1),
-
                   c(3,2,1,3,1,1),
                   c(3,2,2,2,1,1),
                   c(3,2,3,1,1,1),
-
                   c(3,3,1,3,1,1),
                   c(3,3,2,2,1,1),
                   c(3,3,3,1,1,1)
@@ -179,7 +146,7 @@ fis_TA <- addrule(fis_TA,matrix(ruleList,ncol=6))
 # Tudo Lido, modelos criados, vamos executar
 
 # Cria uma entrada para os FIS's
-entrada <- cbind(CN$grid_code,Declividade$grid_code,Permeabilidade$grid_code)
+entrada <- cbind(CN,Declividade,Permeabilidade)
 entrada <- matrix(entrada, ncol = 3)
 
 # Verificaï¿½ï¿½es da Entrada
@@ -192,12 +159,13 @@ resultadoTAgr <- evalfis(entrada,fis_TA)
 resultadoTPerc <- evalfis(entrada,fis_TP)
 
 # Copia o objeto em outro SHP
-shp_TA <- CN
-shp_TP <- CN
+shp_results <- camadas_unidas
 
 # Copia o resultado dos FISs para o Objeto SHP
-shp_TA$grid_code <- resultadoTAgr
-shp_TP$grid_code <- resultadoTPerc
+shp_results$TA <- as.vector(resultadoTAgr)
+shp_results$TP <- as.vector(resultadoTPerc)
 
-st_write(shp_TA, "ShapeFiles/output/shp_TA.shp")
-st_write(shp_TP, "ShapeFiles/output/shp_TP.shp")
+
+objects(shp_results)
+
+st_write(shp_results, "ShapeFiles/output/shp_results.shp")
